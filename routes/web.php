31e94c302f6b125211d322a1f843a280a43118e8
root <root@localhost.localdomain>
2020-09-20 10:37:30 +0800

    add basci routing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+// posts 資料夾下的 list blade 檔，列出所有文章
+Route::get('/posts', function () {
+    return view('posts.list');
+});
+
+// 傳一個 id 進來， /posts/9478
+Route::get('/posts/{id}', function ($id) {
+    return view('posts.show');
 });
